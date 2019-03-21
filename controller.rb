2ce8410a0addc121818db92114da7b29bd9b9b17
itggot-david-petersson <david.petersson@itggot.se>
2019-03-21 11:03:32 +0100

    extra variable

--- controller.rb
+++ controller.rb
@@ -1,13 +1,19 @@
+require 'slim'
+require 'sqlite3'
+require 'sinatra'
+require 'bcrypt'
+require 'byebug'
+
+db = SQLite3::Database.new("db/users.db")
+
 def login()
-    db = SQLite3::Database.new("db/users.db")
     db.results_as_hash = true
     result = db.execute("SELECT Id, Password FROM users WHERE Username = ?", params["username"])
 
 end
 
 def register()
-    db = SQLite3::Database.new("db/users.db")
     db.results_as_hash = true
     hashed_pass = BCrypt::Password.create(params["password"])
-    db.execute("INSERT INTO users(Username, Password) VALUES(?, ?)"params["username"], hashed_pass )
+    db.execute("INSERT INTO users(Username, Password) VALUES(?, ?)"params["username"], hashed_pass)
 end
